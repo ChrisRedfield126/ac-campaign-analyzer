b541db1a8e466d86ff3e0d8dc29c74b27c1ecb31
ChrisRedfield126 <christophe.protat@gmail.com>
2022-12-06 15:07:14 +0100

    Last version

---
+++ latest-compiled-version/precache-manifest.d4eb5413f7292cdd987e2a63de287fef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af355aca12db9f19506a104c6e0f3488",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d9b08b75bc399120e79f",
+    "url": "./static/css/2.8c221055.chunk.css"
+  },
+  {
+    "revision": "e3b0adb10e24807d6f69",
+    "url": "./static/css/main.f40c7f43.chunk.css"
+  },
+  {
+    "revision": "d9b08b75bc399120e79f",
+    "url": "./static/js/2.0305f978.chunk.js"
+  },
+  {
+    "revision": "e3b0adb10e24807d6f69",
+    "url": "./static/js/main.404fc20d.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
